32011b916e25c89d6d90a003bbf39285af97a7d9
Luis Chavez <luis.chavez1996@gmail.com>
2022-08-03 21:46:31 -0700

    Fourth Commit

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,8 +1,8 @@
 
-export default (state = [], action) => {
+export default (posts = [], action) => {
     switch(action.type) {
         case 'FETCH_ALL':
-            return posts;
+            return action.payload;
         case 'CREATE':
             return posts;
         default:
