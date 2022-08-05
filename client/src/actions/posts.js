f13d072400c8b1b5f090e7dfdf8dceaede847e65
Luis Chavez <luis.chavez1996@gmail.com>
2022-08-04 23:20:05 -0700

    6th Commit

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -11,4 +11,14 @@ export const getPosts = () => async(dispatch) => {
     }catch(error){
         console.log(error.message);
     }
-}
+};
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const { data } = await api.creatPost(post);
+
+        dispatch({ type: 'CREATE', payload: data})
+    } catch (error) {
+        console.log(error.message);
+    }
+};
