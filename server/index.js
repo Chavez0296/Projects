dd9d42d0e65e6a18d536dbe7e1274ed1b519ad7e
Luis Chavez <luis.chavez1996@gmail.com>
2022-08-10 15:19:35 -0700

    7th Commit

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/post.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+
+app.use(bodyParser.json({limit: "40mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "40mb", extended: true}));
 app.use(cors());
-
+app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb+srv://cs157:Passw0rd@cs157.j1ymg.mongodb.net/SocialMediaApp?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
